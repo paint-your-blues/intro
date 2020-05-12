f1b714a789eff3d5b09a220773cd9390ca3ae984
MonkeyScript <monkeyscript.dev@gmail.com>
2020-05-12 12:54:22 +0530

    Add smooth scrolling to guidelines section on button click

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
-// FAQ accordion
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FAQ accordion ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 
 var acc = document.getElementsByClassName("accordion");
 var i;
+// Iterate over accordion and add click listeners
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("accordion-active");
@@ -12,7 +15,23 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-
 // Set first collapse to be active by defualt 
 acc[0].classList.toggle("accordion-active");
 acc[0].nextElementSibling.style.maxHeight = acc[0].nextElementSibling.scrollHeight + "px";
+
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Section scrolling ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Reset scroll on reload 
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+// Smooth scrolling 
+var guidelinesSection = document.getElementById('guidelinesSection');
+var guidelinesPosition = guidelinesSection.getBoundingClientRect().top;
+function scrollToGuidelines(){
+   window.scrollTo({
+       top: guidelinesPosition,
+       behavior: "smooth"
+   });
+}
